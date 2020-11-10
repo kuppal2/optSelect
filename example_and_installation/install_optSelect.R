455008aed460250efd697f752fb264af37b29844
Karan Uppal <kuppal2@emory.edu>
2020-11-10 08:09:12 -0500

    v0.0.1

--- example_and_installation/install_optSelect.R
+++ example_and_installation/install_optSelect.R
@@ -1,18 +1,15 @@
 #Step 1: Install dependencies:
-
-
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
-BiocManager::install(c("bioDist","limma"))
-BiocManager::install("CMA",dependencies=TRUE)
-install.packages("expm",dependencies=TRUE)
-install.packages("parallel",dependencies=TRUE)
-install.packages("e1071",dependencies=TRUE)
-install.packages("yaImpute",dependencies=TRUE)
-install.packages("RankAggreg",dependencies=TRUE)
-install.packages("pROC",dependencies=TRUE)
-install.packages("devtools",dependencies=TRUE)
+BiocManager::install(c("bioDist","limma"),update=FALSE,ask=FALSE)
+BiocManager::install("CMA",dependencies=TRUE,update=FALSE,ask=FALSE)
+install.packages("expm",dependencies=TRUE,ask=FALSE,repos="https://cran.r-project.org")
+install.packages("parallel",dependencies=TRUE,ask=FALSE,repos="https://cran.r-project.org")
+install.packages("e1071",dependencies=TRUE,ask=FALSE,repos="https://cran.r-project.org")
+install.packages("yaImpute",dependencies=TRUE,ask=FALSE,repos="https://cran.r-project.org")
+install.packages("RankAggreg",dependencies=TRUE,ask=FALSE,repos="https://cran.r-project.org")
+install.packages("pROC",dependencies=TRUE,ask=FALSE,repos="https://cran.r-project.org")
+install.packages("devtools",dependencies=TRUE,ask=FALSE,repos="https://cran.r-project.org")
 ###############################################
 
 #Step 2: Install optSelect
